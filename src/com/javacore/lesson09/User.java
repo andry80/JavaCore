bcb1031dc59aae597c59a739ef5518680ba76fe9
andry80 <8888appl8888@gmail.com>
2024-09-22 21:12:39 +0300

    lesson09 fix

--- src/com/javacore/lesson09/User.java
+++ src/com/javacore/lesson09/User.java
@@ -2,28 +2,26 @@ package com.javacore.lesson09;
 
 public class User {
 
-    String userName;
-    String userEmail;
-    String userPhoneNumber;
-    String userPassword;
+    private String userName;
+    private String userEmail;
+    private String userPhoneNumber;
+    private String userPassword;
 
-    private User() {
+    public User() {}
 
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    private boolean verificator(String parameter, String regex) throws InvalidInputException {
+    private boolean isValid(String parameter, String regex) throws InvalidInputException {
         if (parameter.matches(regex)) {
             return true;
         }
         throw new InvalidInputException(parameter + " is not in " + regex);
     }
 
+    public String getUserName() {
+        return userName;
+    }
+
     public void setUserName(String userName) throws InvalidInputException {
-        if (this.verificator(userName, "[a-zA-Zа-яА-Я]{3,256}")) {
+        if (this.isValid(userName, "[a-zA-Zа-яА-Я]{3,256}")) {
             this.userName = userName;
         }
     }
@@ -33,7 +31,7 @@ public class User {
     }
 
     public void setUserEmail(String userEmail) throws InvalidInputException {
-        if (this.verificator(userEmail, ".+@.+\\..+")) {
+        if (this.isValid(userEmail, ".+@.+\\..+")) {
             this.userEmail = userEmail;
         }
     }
@@ -43,7 +41,7 @@ public class User {
     }
 
     public void setUserPhoneNumber(String userPhoneNumber) throws InvalidInputException {
-        if (this.verificator(userPhoneNumber, "(-|\\+)?\\d{7,}")) {
+        if (this.isValid(userPhoneNumber, "(-|\\+)?\\d{7,}")) {
             this.userPhoneNumber = userPhoneNumber;
         }
     }
@@ -53,58 +51,9 @@ public class User {
     }
 
     public void setUserPassword(String userPassword) throws InvalidInputException {
-        if (this.verificator(userPassword, "(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}")) {
+        if (this.isValid(userPassword, "(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}")) {
             this.userPassword = userPassword;
         }
-    }
-
-    public static User createUser() {
-        Input input = new Input();
-
-        User user = new User();
-
-        int num = 0;
-        String buffer;
-
-        while (true) {
-            try {
-                switch (num) {
-                    case 0 -> {
-                        buffer = input.inputParameter("name");
-                        user.setUserName(buffer);
-                        System.out.println("successful");
-                        num = 1;
-                    }
-                    case 1 ->  {
-                        user.setUserEmail(input.inputParameter("email"));
-                        System.out.println("successful");
-                        num = 2;
-                    }
-                    case 2 ->  {
-                        user.setUserPhoneNumber(input.inputParameter("phone numer"));
-                        System.out.println("successful");
-                        num = 3;
-                    }
-                    case 3 ->  {
-                        user.setUserPassword(input.inputParameter("password"));
-                        System.out.println("successful");
-                        if (user.userPassword.equals(input.inputParameter("confirm password"))) {
-                            System.out.println("successful");
-                            num = 4;
-                        }else {
-                            System.out.println("password is not correct");
-                            continue;
-                        }
-                    }
-                }
-                if (num == 4) {
-                    break;
-                }
-            } catch (InvalidInputException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-        return user;
     }
 
     @Override
--- src/com/javacore/lesson09/UserRegistration.java
+++ src/com/javacore/lesson09/UserRegistration.java
@@ -2,8 +2,56 @@ package com.javacore.lesson09;
 
 public class UserRegistration {
 
+    public static void inputUserData(User user) {
+        Input input = new Input();
+
+        int num = 0;
+        String buffer;
+
+        while (true) {
+            try {
+                switch (num) {
+                    case 0 -> {
+                        buffer = input.inputParameter("name");
+                        user.setUserName(buffer);
+                        System.out.println("successful");
+                        num = 1;
+                    }
+                    case 1 ->  {
+                        user.setUserEmail(input.inputParameter("email"));
+                        System.out.println("successful");
+                        num = 2;
+                    }
+                    case 2 ->  {
+                        user.setUserPhoneNumber(input.inputParameter("phone number"));
+                        System.out.println("successful");
+                        num = 3;
+                    }
+                    case 3 ->  {
+                        user.setUserPassword(input.inputParameter("password"));
+                        System.out.println("successful");
+                        if (user.getUserPassword().equals(input.inputParameter("confirm password"))) {
+                            System.out.println("successful");
+                            num = 4;
+                        }else {
+                            System.out.println("password is not correct");
+                            continue;
+                        }
+                    }
+                }
+                if (num == 4) {
+                    break;
+                }
+            } catch (InvalidInputException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        User user = User.createUser();
+        User user = new User();
+
+        inputUserData(user);
 
         System.out.println(user);
     }
